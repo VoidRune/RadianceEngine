d95271adf05cf0df72e131cd8e9e1bd6d70456a7
Miha <mhbernik@gmail.com>
2026-08-09 16:42:39 +0200

    added gpubuffer write function

--- RadianceEngine/src/RadianceEngine/Graphics/Resources/GpuBuffer.h
+++ RadianceEngine/src/RadianceEngine/Graphics/Resources/GpuBuffer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "RadianceEngine/Graphics/Handle.h"
 #include "RadianceEngine/Graphics/Common.h"
+#include "RadianceEngine/Core/Log.h"
 
 namespace Rdn
 {
@@ -15,6 +16,26 @@ namespace Rdn
 	class GpuBuffer
 	{
 	public:
+		void Write(const void* data, uint32_t size)
+		{
+			if (size > m_Size)
+			{
+				RDN_LOG_ERROR("Writing to GpuBuffer is invalid, size [{}] is greater than whole size [{}]", size, m_Size);
+			}
+			if (m_MappedPtr == nullptr)
+			{
+				RDN_LOG_ERROR("Writing to GpuBuffer is invalid, mapped pointer is nullptr");
+			}
+			uint8_t* dst = static_cast<uint8_t*>(m_MappedPtr);
+			memcpy(dst, data, size);
+		}
+		template<typename T>
+		void Write(const T& value)
+		{
+			Write(&value, sizeof(T));
+		}
+
+
 		BufferHandle GetHandle() { return m_Buffer; }
 		uint32_t GetSize() { return m_Size; }
 		void* GetMappedPtr() { return m_MappedPtr; }
